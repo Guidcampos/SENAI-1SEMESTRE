001e928cb3a34867d66e34324fb7e97d8b8d0953
Guilherme de Campos <guilherme.campos.r@gmail.com>
2023-03-28 17:22:56 -0300

    Introdução a C#

---
+++ Backend 1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Declarando variaveis
+
+// string nome = "Guilherme"; 
+// const int idade = 26;
+// Console.WriteLine( "a idade de "+ nome +" é igual a "+idade);
+ 
+//  int quantidade = 10;
+//  double preco = 4.99D;
+//  float altura = 1.80F;
+//  bool careca = true;
+//  string texto = "olá, mundo";
+//  char letra = 'C';
+
+
+// operadores aritiméticos 
+
+
+string nome = "Guilherme";
+float peso = 77.5F;
+float altura = 1.83F;
+
+float imc = peso / (altura * altura);
+Console.WriteLine( "o imc de "+nome+" é igual a "+ Math.Round(imc,2) );
+
