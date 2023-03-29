c2a5700f7654391bcd727e2ac960819d832e8e7c
Guilherme de Campos <guilherme.campos.r@gmail.com>
2023-03-29 17:21:03 -0300

    Projetos C# inicio

--- Backend 1/projetoIdade/Program.cs
+++ Backend 1/projetoIdade/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine($"Insira seu ano de nascimento:");
 int nascimento = int.Parse(Console.ReadLine());
 
-int anos = 2023 - nascimento;
+var data = DateTime.Now;
+
+
+int anos = data.Year - nascimento;
 int semanas = anos * 52;
 
 Console.WriteLine($"Seu ano de nascimento é {nascimento}, sua idade é {anos} anos, que é igual a {semanas} semanas");
---
+++ Backend 1/projetoMedia/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine($"Insira sua nota 1:");
+float nota1 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Insira sua nota 2:");
+float nota2 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Insira sua nota 3:");
+float nota3 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Insira sua nota 4:");
+float nota4 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Insira sua nota 5:");
+float nota5 = float.Parse(Console.ReadLine());
+
+
+float media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+Console.WriteLine(@$"
+Nota 1: {nota1}
+Nota 2: {nota2}
+Nota 3: {nota3}
+Nota 4: {nota4}
+Nota 5: {nota5}
+
+Sua media final é: {media}
+");
+
+
+
+
