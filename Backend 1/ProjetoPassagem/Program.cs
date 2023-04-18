a543f666b1af6a980ea86271b01d0ba77b9c829b
Guilherme de Campos <guilherme.campos.r@gmail.com>
2023-04-18 17:25:45 -0300

    att passagem

--- Backend 1/ProjetoPassagem/Program.cs
+++ Backend 1/ProjetoPassagem/Program.cs
@@ -15,6 +15,37 @@ string senhainformada;
 const string senhasistema = "321";
 bool menuopcao = true;
 
+bool nvcad = true;
+bool Stop = false;
+bool menu = false;
+
+
+
+string[] nome = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] data = new string[5];
+
+static void cadastrarPassagens(string[] ns, string[] ori, string[] dest, string[] dt, int pos)
+{
+
+    Console.WriteLine($"Insira seu nome:");
+    ns[pos] = Console.ReadLine()!;
+
+    Console.WriteLine($"Insira a origem do seu voo:");
+    ori[pos] = Console.ReadLine()!;
+
+    Console.WriteLine($"Insira o destino do seu voo:");
+    dest[pos] = Console.ReadLine()!;
+
+    Console.WriteLine($"Insira a data do seu voo: (dd/mm/aa)");
+    dt[pos] = Console.ReadLine()!;
+
+}
+
+
+
+
 
 static bool login(string senhainformada)
 {
@@ -31,16 +62,18 @@ do
 
 Console.WriteLine($"ACESSO LIBERADO");
 
-Console.WriteLine($@"
+char novocad = 'a';
+
+do
+{
+
+    Console.WriteLine($@"
 ------------------------------
            MENU
     1- Cadastrar passagem
     2- Listar Passagens
         0- Sair
 ------------------------------");
-
-do
-{
 
     Console.WriteLine($"Escolha uma opção:");
     char opcao = char.Parse(Console.ReadLine()!);
@@ -49,29 +82,107 @@ do
     {
 
         case '1':
+
+            for (int i = 0; i < 2; i++)
+            {
+                do
+
+                {
+
+
+                    cadastrarPassagens(nome, origem, destino, data, i);
+
+
+
+                    Console.WriteLine($"Deseja realizar mais um cadastro? S/N");
+                    novocad = char.Parse(Console.ReadLine()!.ToUpper());
+
+
+                    switch (novocad)
+                    {
+
+                        case 'S':
+                            break;
+
+                        case 'N':
+                            menu = true;
+                            i = 3;
+                            break;
+
+                        default:
+
+                            break;
+
+                    }
+                } while (novocad == 'S');
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            menu = false;
+            Stop = false;
             menuopcao = true;
             break;
 
-
         case '2':
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine($"Nome: {nome[i]}");
+                Console.WriteLine($"Origem: {origem[i]}");
+                Console.WriteLine($"Destino: {destino[i]}");
+                Console.WriteLine($"Data: {data[i]}");
+
+            }
+
+            Stop = false;
             menuopcao = true;
+            menu = false;
             break;
 
 
         case '0':
+            Stop = false;
             menuopcao = true;
+            menu = false;
             break;
 
         default:
             menuopcao = false;
+            menu = true;
+            Stop = true;
             break;
 
     }
 
+} while (novocad == 'N' && novocad != 'S');
 
 
 
-} while (menuopcao == false);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -86,10 +197,7 @@ do
 // bool Stop = false;  
 
 
-//         string[] nome = new string[5];
-//         string[] origem = new string[5];
-//         string[] destino = new string[5];
-//         string[] data = new string[5];
+
 
 //     for (int i = 0; i < 2; i++)
 //     {
@@ -109,36 +217,36 @@ do
 //             break;
 //         }
 
-//         do
-//         {
-//             Console.WriteLine($"Deseja realizar mais um cadastro? S/N");
-//             char novocad = char.Parse(Console.ReadLine()!.ToUpper());
+// do
+// {
+//     Console.WriteLine($"Deseja realizar mais um cadastro? S/N");
+//     char novocad = char.Parse(Console.ReadLine()!.ToUpper());
 
-//             switch (novocad)
-//             {
+//     switch (novocad)
+//     {
 
-//                 case 'S':
-//                     nvcad = true;
-//                     Stop = false;
-//                     break;
+//         case 'S':
+//             nvcad = true;
+//             Stop = false;
+//             break;
 
-//                 case 'N':
-//                    Stop = true;
-//                     break;
-                    
-
-//                 default:
-//                     nvcad = false;
-//                     break;
-//             }
+//         case 'N':
+//            Stop = true;
+//             break;
 
 
-//         } while (nvcad == false);
+//         default:
+//             nvcad = false;
+//             break;
+//     }
 
-        
-//      if (Stop == true){
-//         break;
-//      }
+
+// } while (nvcad == false);
+
+
+//  if (Stop == true){
+//     break;
+//  }
 
 
 //     }
@@ -148,7 +256,7 @@ do
 
 //     return retorno;
 
-    
+
 // }
 
 // cadastro();
